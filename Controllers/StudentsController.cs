05a944d7327a4199da6f433cb393a4186c70bb1b
Osama Dawabsheh <omad10200099@gmail.com>
2024-08-20 07:49:30 +0300

    first commit

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
